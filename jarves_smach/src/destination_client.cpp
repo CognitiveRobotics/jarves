357114a90f43b3fbb39d3f180ce138bb327f47a0
Priyam Parashar <priyam8parashar@gmail.com>
2016-04-25 18:01:22 -0400

    separated RMS installation from interface files

---
+++ jarves_smach/src/destination_client.cpp
@@ -0,0 +1,25 @@
+//ros specific
+#include <ros/ros.h>
+#include <ros/console.h>
+
+//jarves specific
+#include <jarves/Destination.h>
+
+//c++specific
+#include <string>
+
+bool lookup_callback(jarves::Destination::Request &req, jarves::Destination::Response &res){
+
+    std::string destination_string = req.destination;
+    ROS_INFO("Received destination string = %s\n", destination_string);
+    return true;
+}
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "destination_lookup_server");
+    ros::NodeHandle nh;
+
+    ros::ServiceServer service = nh.advertiseService("destination_lookup", lookup_callback);
+
+    return 0;
+}
